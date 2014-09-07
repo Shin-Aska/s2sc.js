10ac2697b3de25451d0a6dd465c935894484e445
Shin Aska <Orillarichard@gmail.com>
2014-09-07 06:46:48 -0700

    Change license from GPL3 to MPL2

--- consolehandler.js
+++ consolehandler.js
@@ -1,4 +1,4 @@
-// @license magnet:?xt=urn:btih:1f739d935676111cfff4b4693e3816e664797050&dn=gpl-3.0.txt GPL-v3-or-Later
+// @license magnet:?xt=urn:btih:3877d6d54b3accd4bc32f8a48bf32ebc0901502a&dn=mpl-2.0.txt
 
 var consolehandler = {
 
